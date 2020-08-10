d9072c0739de9eefdf36b2dba44af1719cc55618
L0ki <49805006+L0kiii@users.noreply.github.com>
2020-08-10 10:12:13 +0800

    Update HostDetails.py

--- HostDetails.py
+++ HostDetails.py
@@ -289,6 +289,9 @@ if __name__ == '__main__':
                 | | | (_) || | | (_| |/\__/ /| (__| (_| || | | |
                 \_|  \___/ |_|  \__,_|\____/  \___|\__,_||_| |_|
                                                                 
-                                                                
+                
+                blog: https://l0ki.top
+                github: https://github.com/L0kiii/FofaScan
+                usage: python3 HostDetails.py                                                        
 """)
     getData()
